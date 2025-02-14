2339679cd62caf8c095df65a1b2c8f78c48e1b36
Geolffrey Mena <gmjun2000@gmail.com>
2025-02-14 16:51:40 -0600

    feat: sponsorship campaigns

---
+++ script/deployment/09_Deploy_Incentives_CampaignSubscriptionTpl.s.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import { DeployBase } from "script/deployment/00_Deploy_Base.s.sol";
+import { CampaignSubscriptionTpl } from "contracts/incentives/campaigns/sponsored/CampaignSubscriptionTpl.sol";
+
+contract DeployCampaignSubscriptionTpl is DeployBase {
+    function run() external returns (address) {
+        vm.startBroadcast(getAdminPK());
+        address mmc = vm.envAddress("MMC");
+        address ledgerVault = vm.envAddress("LEDGER_VAULT");
+        address assetOwnership = vm.envAddress("ASSET_OWNERSHIP");
+
+        bytes memory creationCode = type(CampaignSubscriptionTpl).creationCode;
+        bytes memory constructorArgs = abi.encode(ledgerVault, assetOwnership, mmc);
+        bytes memory initCode = abi.encodePacked(creationCode, constructorArgs);
+        address subscriptionCampaign = deploy(initCode, "SALT_CAMPAIGN_SUBSCRIPTION_TPL");
+        vm.stopBroadcast();
+
+        _checkExpectedAddress(subscriptionCampaign, "SALT_CAMPAIGN_SUBSCRIPTION_TPL");
+        _logAddress("CAMPAIGN_SUBSCRIPTION", subscriptionCampaign);
+        return subscriptionCampaign;
+    }
+}
